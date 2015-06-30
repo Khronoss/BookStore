a6f570dcd845e2b6fddf45f0d4ccd15d0de375f4
Anthony MERLE <merle.anthony0@gmail.com>
2015-07-01 00:51:50 +0200

    Initial commit, just created the project

---
+++ BookStore/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  BookStore
+//
+//  Created by Anthony MERLE on 01/07/2015.
+//  Copyright (c) 2015 Anthony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
